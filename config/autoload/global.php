4d3471277216bafce1afad4def6c1c0684ec6123
Brice POTE <brice.pote@gmail.com>
2017-02-15 19:25:55 +0100

    Tutos Skeleton ZF2 Problème car le Skeleton est celui du ZF3, 404 sur le module Album

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,15 @@
  * file.
  */
 
+
 return [
-    // ...
+		'db' => [
+				'driver' => 'Pdo',
+				'dsn' => 'mysql:dbname=zf2tutorial;hostname=localhost',
+				'username' => 'root',
+				'password' => '',
+				'driver_options' => [
+						PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+				],
+		],
 ];
